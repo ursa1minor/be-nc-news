9d8772a47e44fc5b47edad55eae60fb7af216e32
tom <tom@michaels-MacBook-Pro.local>
2022-09-07 17:11:58 +0100

    console log removed

--- models/nc-news.models.js
+++ models/nc-news.models.js
@@ -4,7 +4,6 @@ const format = require('pg-format');
 const returnTopics = () => {
     return db.query('SELECT * FROM topics')
     .then((res) => {
-        console.log(res.rows)
         return res.rows;
     })
 }
