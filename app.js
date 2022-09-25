f34f6a4e672798b057c7b531325dcc92acacf05d
tom <tom@michaels-MacBook-Pro.local>
2022-09-25 11:31:36 +0100

    errors moved from app to apiRouter

--- app.js
+++ app.js
@@ -5,46 +5,4 @@ const apiRouter = require("./routes/api-router");
 
 app.use("/", apiRouter);
 
-// 404 table not found / id arg not found getComments
-app.all('/*', (req, res, next) => {
-    res.status(404).send({message: 'Item not found'})
-})
-
-// 404 item not found
-app.use((err, req, res, next) => {
-    if (err.status && err.message) {
-        res.status(err.status).send({ message: err.message})
-    } else {
-        next(err)
-    }
-})
-
-// psql query errors: 
-// get article_id / patch article_id: number is not a number
-app.use((err, req, res, next) => {
-    if (err.code === '22P02') {
-        res.status(400).send({
-            message: 'Bad request'
-        })
-    } else {
-        next(err)
-    }
-})
-
-// psql error: patch article_id: missing key
-app.use((err, req, res, next) => {
-    if (err.code === '23502') {
-        res.status(400).send({
-            message: 'Bad request'
-        })
-    } else {
-        next(err)
-    }
-})
-
-// unhandled error
-app.use((err, req, res, next) => {
-    res.status(500).send({ message: 'Internal Server Error'});
-})
-
 module.exports = app;
--- routes/api-router.js
+++ routes/api-router.js
@@ -17,4 +17,46 @@ apiRouter.use("/api/comments", commentsRouter);
 apiRouter.use("/api/topics", topicsRouter);
 apiRouter.use("/api/users", usersRouter);
 
+// 404 table not found / id arg not found getComments
+apiRouter.all('/*', (req, res, next) => {
+  res.status(404).send({message: 'Item not found'})
+})
+
+// 404 item not found
+apiRouter.use((err, req, res, next) => {
+  if (err.status && err.message) {
+      res.status(err.status).send({ message: err.message})
+  } else {
+      next(err)
+  }
+})
+
+// psql query errors: 
+// get article_id / patch article_id: number is not a number
+apiRouter.use((err, req, res, next) => {
+  if (err.code === '22P02') {
+      res.status(400).send({
+          message: 'Bad request'
+      })
+  } else {
+      next(err)
+  }
+})
+
+// psql error: patch article_id: missing key
+apiRouter.use((err, req, res, next) => {
+  if (err.code === '23502') {
+      res.status(400).send({
+          message: 'Bad request'
+      })
+  } else {
+      next(err)
+  }
+})
+
+// unhandled error
+apiRouter.use((err, req, res, next) => {
+  res.status(500).send({ message: 'Internal Server Error'});
+})
+
 module.exports = apiRouter;
